cfebd7ddacc24331c535b5a2a4139a1383807621
Weihong Guan <weihong.guan@gmail.com>
2013-08-08 00:44:21 +0800

    update time2 functions to C++

--- src/demo.cpp
+++ src/demo.cpp
@@ -11,8 +11,8 @@ void setup() {
 	nvic.configure(TIM2_IRQn, 0, 3, ENABLE);
 	Tim t2(TIM2, RCC_APB1Periph_TIM2, RCC_APB1PeriphClockCmd);
 	t2.init(1000, 1000);
-	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-	TIM_Cmd(TIM2, ENABLE);
+	t2.configureIT(TIM_IT_Update);
+	t2.setState();
 }
 
 void loop() {
